41a6004498baaf6af4ffbd5bad30f205c74ef32c
Jojo <Jojo@192.168.8.101>
2016-05-02 09:36:40 +0800

    Commit 4

--- src/examples/AddedClass.java
+++ src/examples/AddedClass.java
@@ -10,8 +10,27 @@ package examples;
  * @author Jojo
  */
 public class AddedClass {
-
+    private int myAge;
+    private String myName;
+    
     public AddedClass() {
     }
+
+    public int getMyAge() {
+        return myAge;
+    }
+
+    public void setMyAge(int myAge) {
+        this.myAge = myAge;
+    }
+
+    public String getMyName() {
+        return myName;
+    }
+
+    public void setMyName(String myName) {
+        this.myName = myName;
+    }
+    
     
 }
